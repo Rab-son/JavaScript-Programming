2d93952255fc3fe4e57ebc5990b3bbcb07cd2587
Rab-son <rabsonsayendajnr@gmail.com>
2022-10-22 08:38:48 +0200

    data types

--- hello.js
+++ hello.js
@@ -14,3 +14,13 @@ var greeting = "Hello"
 console.log(person);
 console.log("Hello", person);
 console.log(greeting, person);
+
+// Data Types in JavaScript
+/*
+1. String
+2. Number
+3. Null
+4. Undefined
+5. BigInt
+6. Symbol
+*/
