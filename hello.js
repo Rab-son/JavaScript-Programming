cbfbfe02d773d6d9d7b795c9f2477a4621050538
Rab-son <rabsonsayendajnr@gmail.com>
2022-10-22 08:53:08 +0200

    operators examples

--- hello.js
+++ hello.js
@@ -28,12 +28,18 @@ console.log(greeting, person);
 // Operators
 /*
 1. = assignment operator
+-------------------------
+// Arithmetic
 2. + Addition
 3. / Division
-4. * Multiplication 
-5. > Greater >
+4. * Multiplication
+-------------------------
+// Comparison
+5. > Greater 
 6. < Less Than
 7. == Equal To
+-------------------------
+
 8. != Not Equal To
 */
 // Logic Operators
@@ -41,8 +47,17 @@ console.log(greeting, person);
 1. && (AND) Checks for both conditions to be true                   
 2. || (OR)   Checks for atleast one condition to be true
 3. ! (NOT) Returns false if the result is true
-
 */
+
+// Examples
+// console.log(2 + 2);
+// console.log(1 + 2 + 3 + 4 + 5);
+// console.log(20 - 18);
+// console.log(2 * 3);
+// console.log(8 / 1);
+// console.log(3 > 2);
+// console.log(2 > 3);
+// console.log(10 == 10);
 
 
 
