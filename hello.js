295f4e92f450819a74cd549be4d7460bb1572021
Rab-son <rabsonsayendajnr@gmail.com>
2022-10-22 08:35:13 +0200

    variables

--- hello.js
+++ hello.js
@@ -2,4 +2,15 @@
 /*
  Multiline comment
 */
+/* Variables 
+var person;//declaration (not assigned still undefined)
+var person = "Manzy"
+*/
+//Printing to the screen
 console.log('Hello World');
+// Variables
+var person = "Manzy"
+var greeting = "Hello"
+console.log(person);
+console.log("Hello", person);
+console.log(greeting, person);
